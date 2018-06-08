fd6d17a00fae63986f1d8746d6c5afbc1a32912f
Ethan Rowe <redsoxrowe@gmail.com>
2018-06-08 13:25:31 -0400

    Fixed Main mage errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Welcome to the Zoo");
-        public main ;
-        (String[]args){
-            Menu menu = new Menu();
-        }
-            menu.mainMenu();
-        }
+        Menu menu = new Menu();
+        menu.mainMenu();
     }
+}
